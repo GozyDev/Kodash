fc20ad7e1f1ef61ed944e2d835314267c8ae1d5b
GozyDev <networkinggozy@gmail.com>
2026-01-28 07:11:08 +0100

    worked on org header responsiveness

--- components/OrgHeader.tsx
+++ components/OrgHeader.tsx
@@ -17,8 +17,8 @@ export default function OrgHeader({
 }) {
   return (
     <header className="fixed top-0 left-0 w-full flex justify-between items-center  text-neutral-100 px-2 py-3 border-b border-cardCB/80 backdrop-blur-sm ">
-      <div className="flex items-center gap-3">
-        <div className="flex items-center gap-2">
+      <div className="flex items-center gap-1">
+        <div className="">
           <Image
             src="/Logo.png"
             alt="Supabase Logo"
@@ -26,34 +26,30 @@ export default function OrgHeader({
             height={35}
             className="text-white"
           />
-          <span className="font-light text-gray-700/50 text-lg">/</span>
+       
         </div>
 
-        <div className="flex items-center gap-3">
+        <div className="flex items-center gap-1">
           <p className="font-medium text-sm text-gray-100">
             {/* show active org name if present */}
             {orgs.find((o) => o.id === orgId)?.name ?? "Organizations"}
           </p>
 
-          {/* <button className="px-2 text-sm font-light border border-gray-800 rounded-xl transition-colors  capitalize">
-            {orgs.find((o) => o.id === orgId)?.plan ?? "Free Plan"}
-          </button> */}
+         
 
-          <DropDownContent orgs={orgs} />
+          <DropDownContent orgs={orgs} /> 
         </div>
 
         <SidebarTrigger className="bg-cardC hover:bg-cardC/50" />
 
       </div>
 
-      <div className="flex items-center gap-4">
-        <div className="flex items-center gap-3 mr-2">
-          <button className="p-2 text-sm font-light border border-gray-800 rounded-lg hover:bg-gray-800">
+      <div className="flex items-center gap-2">
+        <div className="flex items-center">
+          <button className="p-2 text-[12px] md:text-sm font-light border border-gray-800 rounded-lg hover:bg-gray-800">
             Feedback
           </button>
-          <button className="p-2 rounded-lg hover:bg-gray-800">
-            <Settings className="w-5 h-5 text-gray-400" />
-          </button>
+       
         </div>
 
         <User />
