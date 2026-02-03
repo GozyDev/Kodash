c3ff45aca7dcdaa2f1a10f0df06d8d1d56ddf46a
GozyDev <networkinggozy@gmail.com>
2026-02-03 13:44:45 +0100

    organization card responsiveness

--- app/(Dashboard)/dashboard/(Organization)/organizations/page.tsx
+++ app/(Dashboard)/dashboard/(Organization)/organizations/page.tsx
@@ -126,7 +126,7 @@ export default function OrganizationsPage() {
             <div className="text-textNe">No workspaces found.</div>
           )}
 
-          <div className="grid grid-cols-1  lg:grid-cols-2 xl:grid-cols-3  gap-4">
+          <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3  gap-4">
             {filtered.map((org) => (
               <div
                 key={org.id}
