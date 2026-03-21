48dd4823d14717d4d0488dad7115271923b90c26
GozyDev <networkinggozy@gmail.com>
2026-03-21 19:33:19 +0100

    added class z-[999]

--- components/OrgHeader.tsx
+++ components/OrgHeader.tsx
@@ -15,7 +15,7 @@ export default function OrgHeader({
   orgId?: string;
 }) {
   return (
-    <header className="fixed top-0 left-0 w-full flex justify-between items-center  text-neutral-100 px-2 py-3 border-b border-cardCB/80 bg-bgPrimary/90 backdrop-blur-2xl">
+    <header className="fixed top-0 left-0 w-full flex justify-between items-center  text-neutral-100 px-2 py-3 border-b border-cardCB/80 bg-bgPrimary/90 backdrop-blur-2xl z-[999]">
       <div className="flex items-center gap-1">
         <div className="">
           <Link href="/">
