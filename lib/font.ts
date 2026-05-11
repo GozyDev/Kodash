1b205caafb5b0b08f1efe8dad125b51f6d2ad4dd
GozyDev <networkinggozy@gmail.com>
2026-05-11 10:23:14 +0100

    Add Alfa Slab One font import to enhance typography options

--- lib/font.ts
+++ lib/font.ts
@@ -1,6 +1,14 @@
 import { Poppins } from "next/font/google";
+import { Alfa_Slab_One} from "next/font/google";
+
 
 export const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500"],
   subsets: ["latin"],
 });
+
+export const alfa_slab_one  = Alfa_Slab_One({
+  weight: ["400"],
+  subsets: ["latin"],
+});
+
